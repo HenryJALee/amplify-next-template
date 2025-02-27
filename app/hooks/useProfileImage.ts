488058b69aee64667dcfcb64daf5a9b0f7074970
HenryJALee <henryjaleegmail.com>
2025-02-27 12:11:52 -0800

    this is it

--- app/hooks/useProfileImage.ts
+++ app/hooks/useProfileImage.ts
@@ -40,7 +40,7 @@ export const useProfileImage = ({ userData, onUpdateUser }:UseProfileImageProps)
         
         setProfileImage({
           url: urlResult.url.href,
-          path: userData.profileImageKey
+          key: userData.profileImageKey
         });
       } catch (error) {
         console.error('Error loading profile image:', error);
